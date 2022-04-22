fbcd2941ab42a24e4e0d9e6fd98f7ade102b97cd
Vinay0700 <89821304+Vinay0700@users.noreply.github.com>
2022-04-22 13:40:00 +0530

    Time: 4 ms (96.13%), Space: 8.4 MB (58.63%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
-        //SLIDING WINDOW  - TIME COMPLEXITY O(2n)
-        //                  SPACE COMPLEXITY O(m)   //size of array
-        
-        int store[256]={0}; //array to store the occurences of all the characters
-        int l=0;    //left pointer
-        int r=0;    //right pointer
-        int ans=0;  //initializing the required length as 0
-        
-        while(r<s.length())     //iterate over the string till the right pointer reaches the end of the string 
-        {
-            store[s[r]]++;      //increment the count of the character present in the right pointer 
-            
-            while(store[s[r]]>1)    //if the occurence become more than 1 means the char is repeated
-            { 
-                store[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
-                l++;         //contraction of the present window till the occurence of the 't' char becomes 1
-            }
-            
-            ans = max(ans,r-l+1);    //As the index starts from 0 , ans will be (right pointer-left pointer + 1)
-            r++;        // now will increment the right pointer 
+        vector<int> mpp(256,-1);
+       int length = s.size();
+       int left =0,right =0;
+        int answer=0;
+        while(right < length){
+            if(mpp[s[right]] != -1) left= max(mpp[s[right]]+1,left);
+            mpp[s[right]]=right;
+            answer=max(answer,right-left+1);
+            right++;
         }
-        return ans;
-    }
+        return answer;}
+        
 };
