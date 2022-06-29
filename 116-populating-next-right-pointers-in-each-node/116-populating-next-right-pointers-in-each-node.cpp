3ab5bd4b700d65579208c8cd362acba0af867a64
Vinay0700 <89821304+Vinay0700@users.noreply.github.com>
2022-06-29 23:43:28 +0530

    Time: 40 ms (19.00%), Space: 16.7 MB (96.62%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return nullptr;
+        auto L = root -> left, R = root -> right, N = root -> next;
+        if(L) {
+            L -> next = R;                                // next of root's left is assigned as root's right
+            if(N) R -> next = N -> left;                  // next of root's right is assigned as root's next's left (if root's next exist)
+            connect(L);                                   // recurse left  - simple DFS 
+            connect(R);                                   // recurse right
+        }
+        return root;
+    }
+};
