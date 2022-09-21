f4c67358aa825e6f11727aadd94648f21f5acd15
Vinay0700 <89821304+Vinay0700@users.noreply.github.com>
2022-09-21 23:52:21 +0530

    Time: 235 ms (28.37%), Space: 45.2 MB (97.42%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
-    vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& qs, vector<int> res = {}) {
-  int sum = accumulate(begin(A), end(A), 0, [](int s, int a) { return s + (a % 2 == 0 ? a : 0); });
-  for (auto &q : qs) {
-    if (A[q[1]] % 2 == 0) sum -= A[q[1]];
-    A[q[1]] += q[0];
-    if (A[q[1]] % 2 == 0) sum += A[q[1]];
-    res.push_back(sum);
-  }
-  return res;
-}
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+
+        int sum = 0, N = queries.size();
+        for (int n : nums) {
+            if (n % 2 == 0) sum += n;
+        }
+        
+        vector<int> ans(N, 0);
+        
+        // Four cases to handle.
+        for (int i = 0; i < N; i++) {
+            int val = queries[i][0], index = queries[i][1];
+            int oldValue = nums[index];
+            nums[index] += val;
+            
+            bool wasEven = (oldValue % 2) == 0;
+            bool nowEven = (nums[index] % 2 == 0);
+            
+            if (wasEven && nowEven) {
+                sum += val;
+                ans[i] = sum;
+            } else if (!wasEven && nowEven) {
+                sum += nums[index];
+                ans[i] = sum;
+            } else if (wasEven && !nowEven) {
+                sum -= oldValue;
+                ans[i] = sum;
+            } else {
+                ans[i] = sum;
+            }
+        }
+        
+        return ans;
+    }
 };
